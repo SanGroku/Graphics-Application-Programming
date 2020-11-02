68c921957255a6db02c03645c55703073005707f
SanGroku <37126121+SanGroku@users.noreply.github.com>
2020-11-01 22:13:15 -0600

    Menu Revise
    Finalizes the menu renovations made last week. Adds a cool bouncing ball to the menu as well; however. it bounces really, really slowly...
    
    Closes #1
    Closes #3

---
+++ 3D Animation References/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    //Load the bouncing ball scene
+    public void LoadBouncingBall()
+    {
+        SceneManager.LoadScene("Bouncing Ball");
+    }
+
+    //Load the pendulum scene
+    public void LoadPendulum()
+    {
+        SceneManager.LoadScene("Pendulum");
+    }
+}
