5edac3c5358e4b309c803b4d7335fded307d7a68
SanGroku <37126121+SanGroku@users.noreply.github.com>
2020-11-05 12:58:18 -0600

    Re: Ball Bounce
    Remakes the bouncing ball.
    
    Problem:
    The scale of the ball was causing it to go way too slow. Was not sure how to speed it up.
    
    Solution:
    Make the ball smaller!

---
+++ 3D Animation References/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float moveSpeed;
+    public float gravity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+
+        rb.velocity = movement.normalized * moveSpeed * Time.deltaTime;
+
+    }
+}
